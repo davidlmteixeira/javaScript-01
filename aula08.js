00441ee996d369f7b608ebb6f7868a1e5c4362b5
David Luis <92178929+davidlmteixeira@users.noreply.github.com>
2022-07-27 20:50:16 -0300

    Create aula08.js

---
+++ aula08.js
@@ -0,0 +1,23 @@
+/*  operadores relacionais, logico e ternario
+
+ conjução     true && true = true
+              true && false = false
+              false && true = false 
+              false && false = false
+
+ disjunção    true || true = true
+              true || false = true
+              false || true = true
+              false || false = false
+
+ negação      !true = false
+              !false = true   */
+
+/* Ordem de precendencia 
+() ** /     1° operadores aritmeticos
+> < >=      2° operadores relacionais
+!           3° operadores logicos
+&&          4° operadores logicos
+||          5° operadores logicos 
+?:          6° operadores ternarios (teste ? true : false)
+ */
